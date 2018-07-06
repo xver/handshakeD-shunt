54d03d3aa8683e09f44933ecc643b7328871cb25
xver <victor.besyakov@gmail.com>
2018-07-05 22:06:52 -0400

    print update

--- include/shunt_fringe_if.sv
+++ include/shunt_fringe_if.sv
@@ -14,44 +14,45 @@ interface shunt_fringe_if ();
 `include "cs_common.svh"
    import shunt_dpi_pkg::*;
   
-   typedef enum  {TARGET_IDLE, TARGET_ACTIVE, TARGET_FINISH,TARGET_STOP} target_status_e;
-   typedef enum  {DATA_IDLE,DATA_VALID_GET,DATA_VALID_PUT} data_valid_e;
-   
+   typedef enum  {FRNG_TARGET_IDLE, FRNG_TARGET_ACTIVE, FRNG_TARGET_FINISH,FRNG_TARGET_STOP} fringe_target_status_e;
+   typedef enum  {FRNG_DATA_IDLE,FRNG_DATA_VALID_GET,FRNG_DATA_VALID_PUT} fringe_data_valid_e;
+   typedef enum  {FRNG_NO_PRINT,FRNG_PRINT} fringe_signal_debug_e;
 
    /* verilator lint_off UNPACKED */ 
    typedef struct{ 
       longint 	 target_id;
       int 	 n_signals;
-      target_status_e   status;        
-   } targets_descriptor_t;
+      fringe_target_status_e   status;        
+   } fringe_targets_descriptor_t;
    
    typedef struct{ 
       longint 	    data_type;
       longint 	    signal_id;
-      int 	    n_payloads;
+      int 	    signal_size;//n_payloads;
       int           index_payloads_db;
-      data_valid_e  data_valid;
+      fringe_data_valid_e  data_valid;
       int 	    timestamp;
       int           event_cntr;
-   } signals_descriptor_t;
+      fringe_signal_debug_e debug;
+   } fringe_signals_descriptor_t;
    
    /* verilator lint_on UNPACKED */
    
    //
-   typedef enum{FRINGE_REG_REQ,
-		FRINGE_REG_ACK,
-		FRINGE_PUT,
-		FRINGE_GET
+   typedef enum{FRNG_REG_REQ,
+		FRNG_REG_ACK,
+		FRNG_PUT,
+		FRNG_GET
 		} fringe_protocol_e;
    
    //Sim DB
-   signals_descriptor_t Signal;
-   targets_descriptor_t Target;
+   fringe_signals_descriptor_t Signal;
+   fringe_targets_descriptor_t Target;
    //Targets DB
-   targets_descriptor_t targets_db[`N_OF_TARGETS];
+   fringe_targets_descriptor_t targets_db[`N_OF_TARGETS];
    string 	__targets_name_db [`N_OF_TARGETS];
    //Signals DB
-   signals_descriptor_t signals_db            [`N_OF_SIGNALS];
+   fringe_signals_descriptor_t signals_db            [`N_OF_SIGNALS];
    string 	        __signals_target_name_db[`N_OF_SIGNALS];
    string 	        __signals_name_db       [`N_OF_SIGNALS];
    //Paylod db
@@ -111,10 +112,10 @@ interface shunt_fringe_if ();
       for(int i=0;i<`N_OF_TARGETS;i++) begin
 	 targets_db[i].target_id  = {$random,$random};
 	 targets_db[i].n_signals  = targets_n_signals[i];
-	 targets_db[i].status     = TARGET_IDLE; //not active
+	 targets_db[i].status     = FRNG_TARGET_IDLE; //not active
 	 //   
 	 __targets_name_db[i]       = targets[i];
-	 if (__targets_name_db[i] == "INITIATOR") targets_db[i].status = TARGET_ACTIVE;
+	 if (__targets_name_db[i] == "INITIATOR") targets_db[i].status = FRNG_TARGET_ACTIVE;
       end
       return success; 
      endfunction : init_targets_db
@@ -132,7 +133,7 @@ interface shunt_fringe_if ();
       return success; 
    endfunction : print_targets_db
    
-   function  print_target_descr(input targets_descriptor_t Trg,input string Name="Trg");
+   function  print_target_descr(input fringe_targets_descriptor_t Trg,input string Name="Trg");
       $display("%s %s.target_id  =%0h",i_am,Name,Trg.target_id);
       $display("%s %s.n_signals =%0d",i_am,Name,Trg.n_signals);
       $display("%s %s.status =%s",i_am,Name,Trg.status.name()); 
@@ -159,7 +160,7 @@ interface shunt_fringe_if ();
       int idle_entry=-1;
       //find idle entry
       for(int i=0;i<`N_OF_TARGETS;i++) begin
-	 if (targets_db[i].status==TARGET_IDLE && idle_entry<0) idle_entry = i;
+	 if (targets_db[i].status==FRNG_TARGET_IDLE && idle_entry<0) idle_entry = i;
       end // for (int i=0;i<`N_OF_TARGETS;i++)
       return idle_entry; 
    endfunction : check_idle_entry_targets_db
@@ -229,15 +230,17 @@ interface shunt_fringe_if ();
 	begin
 	   signals_db[i].data_type  = shunt_dpi_hash(signals_type[i]);
 	   signals_db[i].signal_id  = shunt_dpi_hash({__signals_target_name_db[i],".",__signals_name_db[i]});
-	   signals_db[i].n_payloads = signals_n_payload[i];
-	   signals_db[i].data_valid = DATA_IDLE;
+	   signals_db[i].signal_size = signals_n_payload[i];
+	   signals_db[i].data_valid = FRNG_DATA_IDLE;
 	   signals_db[i].timestamp  = 0;
 	   signals_db[i].event_cntr = 0;
+	   signals_db[i].debug =FRNG_NO_PRINT;
+ 
 	   //payload initialisation
 	   //find free payload entry
 	   index_payload = check_free_entry_payloads_db();
 	   if(index_payload>=0) begin
-	      for(j=index_payload;j<index_payload+signals_db[i].n_payloads;j++) begin
+	      for(j=index_payload;j<index_payload+signals_db[i].signal_size;j++) begin
 		 /* verilator lint_off WIDTH */
 		 data_payloads_db[j] = signals_db[i].signal_id;
 		 /* verilator lint_on WIDTH */
@@ -260,35 +263,37 @@ interface shunt_fringe_if ();
       success = 1;
             
       for(int i=0;i<`N_OF_SIGNALS;i++) begin
-	 //print_target_descr(targets_db[i]);
-	 $display("%s __signals_name_db[%0d]       = %s",i_am,i,__signals_name_db[i]);
-	 $display("%s __signals_target_name_db[%0d]= %s",i_am,i,__signals_target_name_db[i]);
-	 $display("%s signals_db[%0d].data_type  = %0h",i_am,i,signals_db[i].data_type);
-	 $display("%s signals_db[%0d].signal_id  = %0h",i_am,i,signals_db[i].signal_id);
-	 $display("%s signals_db[%0d].index_payloads_db = %0d",i_am,i,signals_db[i].index_payloads_db);
-	 $display("%s signals_db[%0d].data_valid = %s",i_am,i,signals_db[i].data_valid.name());
-	 $display("%s signals_db[%0d].timestamp  = %0h",i_am,i,signals_db[i].timestamp);
-	 $display("%s signals_db[%0d].event_cntr = %0h",i_am,i,signals_db[i].event_cntr);
-	 //
-	 index_payload = signals_db[i].index_payloads_db;
-	 for(j=index_payload;j<index_payload+signals_db[i].n_payloads;j++) begin
-	    $display("%s data_payloads_db[%0d]=%h",i_am,j,data_payloads_db[j]);
-	    $display("%s signal_id_payloads_db[%0d]=%h",i_am,j,signal_id_payloads_db[j]);
-	 end
+	 if (signals_db[i].debug == FRNG_PRINT)
+	   begin
+	      //print_target_descr(targets_db[i]);
+	      $display("%s __signals_name_db[%0d]       = %s",i_am,i,__signals_name_db[i]);
+	      $display("%s __signals_target_name_db[%0d]= %s",i_am,i,__signals_target_name_db[i]);
+	      $display("%s signals_db[%0d].data_type  = %0h",i_am,i,signals_db[i].data_type);
+	      $display("%s signals_db[%0d].signal_id  = %0h",i_am,i,signals_db[i].signal_id);
+	      $display("%s signals_db[%0d].index_payloads_db = %0d",i_am,i,signals_db[i].index_payloads_db);
+	      $display("%s signals_db[%0d].data_valid = %s",i_am,i,signals_db[i].data_valid.name());
+	      $display("%s signals_db[%0d].timestamp  = %0h",i_am,i,signals_db[i].timestamp);
+	      $display("%s signals_db[%0d].event_cntr = %0h",i_am,i,signals_db[i].event_cntr);
+	      //
+	      index_payload = signals_db[i].index_payloads_db;
+	      for(j=index_payload;j<index_payload+signals_db[i].signal_size;j++) begin
+		 $display("%s data_payloads_db[%0d]=%h",i_am,j,data_payloads_db[j]);
+		 $display("%s signal_id_payloads_db[%0d]=%h",i_am,j,signal_id_payloads_db[j]);
+	      end
+	   end // if (signals_db[i].debug == FRNG_PRINT)
       end // for (int i=0;i<`N_OF_SIGNALS;i++)
-      
       return success; 
    endfunction : print_signals_db
    
 
-    function  print_signal_descr(input signals_descriptor_t Sig,input string Name="Sig");
-       $display("%s %s.data_type  = %0h",i_am,Name,Sig.data_type);
-       $display("%s %s.signal_id  = %0h",i_am,Name,Sig.signal_id);
-       $display("%s %s.n_payloads = %0h",i_am,Name,Sig.n_payloads);
-       $display("%s %s.index_payloads_db = %0h",i_am,Name,Sig.index_payloads_db);
-       $display("%s %s.data_valid = %s",i_am,Name,Sig.data_valid.name());
-       $display("%s %s.timestamp  = %0h",i_am,Name,Sig.timestamp);
-       $display("%s %s.event_cntr = %0h",i_am,Name,Sig.event_cntr);
+   function  print_signal_descr(input fringe_signals_descriptor_t Sig,input string Name="Sig");
+      $display("%s %s.data_type  = %0h",i_am,Name,Sig.data_type);
+      $display("%s %s.signal_id  = %0h",i_am,Name,Sig.signal_id);
+      $display("%s %s.signal_size = %0h",i_am,Name,Sig.signal_size);
+      $display("%s %s.index_payloads_db = %0h",i_am,Name,Sig.index_payloads_db);
+      $display("%s %s.data_valid = %s",i_am,Name,Sig.data_valid.name());
+      $display("%s %s.timestamp  = %0h",i_am,Name,Sig.timestamp);
+      $display("%s %s.event_cntr = %0h",i_am,Name,Sig.event_cntr);
     endfunction : print_signal_descr
 
     function  int check_free_entry_payloads_db();
@@ -489,40 +494,40 @@ interface shunt_fringe_if ();
    function bit target_registration_request(inout cs_header_t h_,input string target_name);
       bit    success;
       success = 0;
-      h_.trnx_type  = shunt_dpi_hash("FRINGE_REG_REQ");
+      h_.trnx_type  = shunt_dpi_hash("FRNG_REG_REQ");
       h_.data_type  = shunt_dpi_hash("SHUNT_HEADER_ONLY");
       h_.trnx_id    = shunt_dpi_hash(target_name);
       h_.n_payloads = sim_id;
       shunt_dpi_send_header(my_socket,h_);
       //
       shunt_dpi_recv_header(my_socket,h_);
-      if (h_.trnx_type == shunt_dpi_hash("FRINGE_REG_ACK") && h_.n_payloads == sim_id && h_.trnx_id >= 0 ) success = 1;
+      if (h_.trnx_type == shunt_dpi_hash("FRNG_REG_ACK") && h_.n_payloads == sim_id && h_.trnx_id >= 0 ) success = 1;
       return success;
    endfunction : target_registration_request
    
    
    function bit initiator_registration(inout cs_header_t h_);
       bit    success;
-      targets_descriptor_t Trg;
+      fringe_targets_descriptor_t Trg;
       int    index;
       
       success = 0;
       shunt_dpi_recv_header(my_socket,h_);
       
       index = get_index_by_hash_targets_db(h_.trnx_id);
-      if (h_.trnx_type ==shunt_dpi_hash("FRINGE_REG_REQ") && h_.n_payloads == sim_id)
+      if (h_.trnx_type ==shunt_dpi_hash("FRNG_REG_REQ") && h_.n_payloads == sim_id)
 	begin 
-	   h_.trnx_type  = shunt_dpi_hash("FRINGE_REG_ACK");
-	   if(index >= 0 && targets_db[index].status==TARGET_IDLE) h_.trnx_id = targets_db[index].target_id;
+	   h_.trnx_type  = shunt_dpi_hash("FRNG_REG_ACK");
+	   if(index >= 0 && targets_db[index].status==FRNG_TARGET_IDLE) h_.trnx_id = targets_db[index].target_id;
 	   else  h_.trnx_id = -1;
 	   h_.data_type  = shunt_dpi_hash("SHUNT_HEADER_ONLY");
 	   h_.n_payloads = sim_id;
 	   shunt_dpi_send_header(my_socket,h_);
 	   if(h_.trnx_id>=0) begin
 	      success = 1;
-	      targets_db[index].status=TARGET_ACTIVE;
+	      targets_db[index].status=FRNG_TARGET_ACTIVE;
 	     end
-	end // if (h_.trnx_type ==shunt_dpi_hash("FRINGE_REG_REQ") && h_.n_payloads == sim_id)
+	end // if (h_.trnx_type ==shunt_dpi_hash("FRNG_REG_REQ") && h_.n_payloads == sim_id)
       return success;
    endfunction : initiator_registration
 
@@ -536,11 +541,11 @@ interface shunt_fringe_if ();
       $display("\n (print_reg_header) %s:",i_am);
       S="NON";
        
-      //$display("FRINGE_REG_ACK =%d <> %d",shunt_dpi_hash("FRINGE_REG_ACK"),h_.trnx_type);
-      if (h_.trnx_type == shunt_dpi_hash("FRINGE_REG_ACK")) S="FRINGE_REG_ACK";
-      if (h_.trnx_type == shunt_dpi_hash("FRINGE_PUT")) S="FRINGE_PUT";
-      if (h_.trnx_type == shunt_dpi_hash("FRINGE_GET")) S="FRINGE_GET";
-      if (h_.trnx_type == shunt_dpi_hash("FRINGE_REG_REQ")) S="FRINGE_REG_REQ";
+      //$display("FRNG_REG_ACK =%d <> %d",shunt_dpi_hash("FRNG_REG_ACK"),h_.trnx_type);
+      if (h_.trnx_type == shunt_dpi_hash("FRNG_REG_ACK")) S="FRNG_REG_ACK";
+      if (h_.trnx_type == shunt_dpi_hash("FRNG_PUT")) S="FRNG_PUT";
+      if (h_.trnx_type == shunt_dpi_hash("FRNG_GET")) S="FRNG_GET";
+      if (h_.trnx_type == shunt_dpi_hash("FRNG_REG_REQ")) S="FRNG_REG_REQ";
       if (h_.trnx_type == shunt_dpi_hash("NA")) S="NA";
       //
       $display("trnx_type = (%d)%s",h_.trnx_type,S);
@@ -580,47 +585,47 @@ interface shunt_fringe_if ();
       int 	  i;
       
       //
-      s_me = "fringe_put";
+      s_me = "fringe_put()";
       success =1;
       //
-      h_.trnx_type  = shunt_dpi_hash("FRINGE_PUT");
+      h_.trnx_type  = shunt_dpi_hash("FRNG_PUT");
       h_.data_type  = -1;
       h_.trnx_id    = -1;
       h_.n_payloads = -1;
       //
       index = get_index_by_name_signals_db(destination_name,signal_name);
-      $display("%s index=%0d destination_name=%s,signal_name=%s",s_me,index,destination_name,signal_name);
+      //$display("%s index=%0d destination_name=%s,signal_name=%s",s_me,index,destination_name,signal_name);
      
       index_payload = signals_db[index].index_payloads_db;
       //
       if(index>=0) begin 
-	 h_.trnx_type  = shunt_dpi_hash("FRINGE_PUT");
+	 h_.trnx_type  = shunt_dpi_hash("FRNG_PUT");
 	 h_.data_type  = signals_db[index].data_type;
 	 h_.trnx_id    = signals_db[index].signal_id;
-	 h_.n_payloads = signals_db[index].n_payloads;
+	 h_.n_payloads = signals_db[index].signal_size;
       end
       
       //
-      print_reg_header(h_);
+      //print_reg_header(h_);
       
       shunt_dpi_send_header(my_socket,h_);
       
       
-      for(i=index_payload;i<index_payload+signals_db[index].n_payloads;i++) begin    
+      for(i=index_payload;i<index_payload+signals_db[index].signal_size;i++) begin    
 	 /* verilator lint_off WIDTH */
 	 data_ =  data_in_t'(data_payloads_db[i]);
 	 /* verilator lint_on WIDTH */
 	 $display("%s data_=%h data_payloads_db[%0d]= %h",s_me,data_,i,data_payloads_db[i]);
 	 Result = shunt_dpi_send_long(my_socket,data_);
 	 if (Result<=0)  success =0;
-      end // for (i=index_payload;i<index_payload+signals_db[index].n_payloads;i++)
+      end // for (i=index_payload;i<index_payload+signals_db[index].signal_size;i++)
       
       shunt_dpi_recv_header(my_socket,h_);//ACK
-      if(h_.trnx_type != shunt_dpi_hash("FRINGE_GET")) success =0;
+      if(h_.trnx_type != shunt_dpi_hash("FRNG_GET"))        success =0;
       if(h_.trnx_id   != signals_db[index].signal_id)       success =0;
       if(h_.n_payloads <  0)                                success =0;
       //
-      signals_db[index].data_valid = DATA_IDLE;
+      signals_db[index].data_valid = FRNG_DATA_IDLE;
       return success;
       
    endfunction : fringe_put
@@ -651,26 +656,26 @@ interface shunt_fringe_if ();
       index_payload = signals_db[index].index_payloads_db;
       if (index_payload <0 )  success =0;
       //      
-      $display("%s index=%0d ",s_me,index);
+      //$display("%s index=%0d ",s_me,index);
       //
       
-      for(i=index_payload;i<index_payload+signals_db[index].n_payloads;i++) begin    
+      for(i=index_payload;i<index_payload+signals_db[index].signal_size;i++) begin    
 	 Result = shunt_dpi_recv_long(my_socket,data_);
 	 /* verilator lint_off WIDTH */
 	 if (Result>0) data_payloads_db[i] =  longint'(data_);
 	 /* verilator lint_on WIDTH */
 	 else  success =0;
 	 $display("%s data_=%h data_payloads_db[%0d]= %h",s_me,data_,i,data_payloads_db[i]);
-      end // for (i=index_payload;i<index_payload+signals_db[index].n_payloads;i++)
+      end // for (i=index_payload;i<index_payload+signals_db[index].signal_size;i++)
            
-      if (!success) h_.trnx_type = shunt_dpi_hash("FRINGE_GET");
+      if (!success) h_.trnx_type = shunt_dpi_hash("FRNG_GET");
       /* verilator lint_off WIDTH */
       h_.trnx_id = signals_db[index].signal_id;
       h_.n_payloads = -1;//FINGE_GET ERROR
       /* verilator lint_on WIDTH */
       shunt_dpi_send_header(my_socket,h_);
       //
-      signals_db[index].data_valid = DATA_VALID_GET;
+      signals_db[index].data_valid = FRNG_DATA_VALID_GET;
       
       return success;
    endfunction : fringe_get
@@ -698,10 +703,10 @@ interface shunt_fringe_if ();
      
      index = get_index_by_name_signals_db(destination_name,signal_name);
      //
-     h_.trnx_type  = shunt_dpi_hash("FRINGE_GET");
+     h_.trnx_type  = shunt_dpi_hash("FRNG_GET");
      h_.data_type  = signals_db[index].data_type;
      h_.trnx_id    = signals_db[index].signal_id;
-     h_.n_payloads = signals_db[index].n_payloads;
+     h_.n_payloads = signals_db[index].signal_size;
      //
      shunt_dpi_send_header(my_socket,h_);
      shunt_dpi_recv_long(my_socket,data_);
