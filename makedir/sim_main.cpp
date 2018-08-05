f3c4737a6f4e0848f8c9b0e803e00163db593891
xver <victor.besyakov@gmail.com>
2018-08-05 11:24:03 -0400

    fix vcd clk finish add clc_i cnt

--- makedir/sim_main.cpp
+++ makedir/sim_main.cpp
@@ -6,27 +6,40 @@
 int main(int argc, char **argv, char **env) {
   int i;
   int clk_main;
+  // Current simulation time (64-bit unsigned)
+  vluint64_t main_time = 0;
+
   Verilated::commandArgs(argc, argv);
   // init top verilog instance
   Vshunt_verilator_dpi* top = new Vshunt_verilator_dpi;
   // init trace dump
   Verilated::traceEverOn(true);
   VerilatedVcdC* tfp = new VerilatedVcdC;
-  top->trace (tfp, 1000);
+  top->trace (tfp, 1000);// Trace 1000 levels of hierarchy
   tfp->open ("top_sim.vcd");
   // initialize simulation inputs
   top->clk_i = 1;
-  // run simulation for 1000 clock periods
-  for (i=0; i<1000; i++) {
-    // dump variables into VCD file and toggle clock
-    for (clk_main=0; clk_main<2; clk_main++) {
-      tfp->dump (2*i+clk_main);
-      top->clk_i = !top->clk_i;
-      top->eval ();
-    }
-    if (Verilated::gotFinish())  exit(0);
+  while (!Verilated::gotFinish()) { 
+    top->clk_i = !top->clk_i;
+    top->eval(); 
+    tfp->dump(main_time);
+    main_time++;  // Time passes...
   }
-  tfp->close();
+  // Destroy model
+  if (tfp) { tfp->close(); tfp = NULL; }
+  delete top; top = NULL;
   exit(0);
+  //   // run simulation for 1000 clock periods
+  //   for (i=0; i<1000; i++) {
+  //     // dump variables into VCD file and toggle clock
+  //     for (clk_main=0; clk_main<2; clk_main++) {
+  //       tfp->dump (2*i+clk_main);
+  //       top->clk_i = !top->clk_i;
+  //       top->eval ();
+  //     }
+  //     if (Verilated::gotFinish())  exit(0);
+  //   }
+  //   tfp->close();
+  //exit(0);
 }
 
